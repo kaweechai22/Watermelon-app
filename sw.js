13562b2106bc1c2d6da5599262c8ea9848dd4c7d
kaweechai22 <cacarosjampa@gmail.com>
2025-08-04 10:08:36 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,20 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('watermelon-app').then(function(cache) {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('melon-cache').then(function(cache) {
       return cache.addAll([
-        'index.html',
-        'manifest.json'
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
- 
